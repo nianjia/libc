a86d51ca69821d798a6b415d524c0aad2cba3140
Pengfei Zhang <zpfalpc23@gmail.com>
2019-04-24 06:46:54 +0800

    Test code

--- src/lib.rs
+++ src/lib.rs
@@ -103,7 +103,7 @@ cfg_if! {
     } else if #[cfg(target_os = "switch")] {
         mod switch;
         pub use switch::*;
-    } else if #[cfg(unix)] {
+    } else if #[cfg(any(unix, target_env = "nianjia"))] {
         mod unix;
         pub use unix::*;
     } else if #[cfg(target_os = "hermit")] {
@@ -115,10 +115,7 @@ cfg_if! {
     } else if #[cfg(target_env = "wasi")] {
         mod wasi;
         pub use wasi::*;
-    } else if #[cfg(target_env = "nianjia")] {
-        mod nianjia;
-        pub use nianjia::*;
-    } else {
+    }  else {
         // non-supported targets: empty...
     }
 }
